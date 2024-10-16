0beb8fc2b97d73870e55d6cff57ee814832afb95
joaonakao <joao_nakao@hotmail.com>
2024-10-16 16:19:48 -0300

    Update script.js

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -23,3 +23,24 @@ function calcAge(birthYear){
 
 const firstName = 'Jonas';
 calcAge(1991);
+
+const jonas = {
+    year: 1991,
+    calcAge: function(){
+        console.log(this);
+        console.log(2037 - this.year);
+    }
+};
+
+jonas.calcAge();
+
+const matilda = {
+    year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+
+f();
