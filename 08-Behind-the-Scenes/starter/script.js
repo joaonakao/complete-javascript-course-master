e6e58c15997129a027f750a4a1e2ada386ccdd42
joaonakao <joao_nakao@hotmail.com>
2024-10-17 13:10:40 -0300

    estudos

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -25,22 +25,81 @@ const firstName = 'Jonas';
 calcAge(1991);
 
 const jonas = {
+    firstName: 'Jonas',
     year: 1991,
     calcAge: function(){
         console.log(this);
         console.log(2037 - this.year);
+
+        const isMillenial = () => {
+        console.log(this.year >= 1981 && this.year <= 1996);
+        }
+        isMillenial();
     }
+
 };
 
 jonas.calcAge();
 
-const matilda = {
-    year: 2017,
+const addExpr = function (a, b) {
+    console.log(arguments);
+    return a + b;
+}
+
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+let age = 30;
+let oldAge = age;
+
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+    name: 'Jonas',
+    age: 30,
+}
+
+const friend = me;
+friend.age = 27;
+
+console.log('friend', friend);
+console.log('me', me);
+
+// pimitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName);
+console.log(oldLastName);
+
+//reference types
+const jessica = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27,
 };
 
-matilda.calcAge = jonas.calcAge;
-matilda.calcAge();
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
 
-const f = jonas.calcAge;
+//copying objects
+const jessica2 = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27,
+    family: ['Alice']
+};
 
-f();
+//object.assign only work in the first level.
+const jessicaCopy = Object.assign({}, jessica2)
+jessicaCopy.lastName = 'Davis';
+console.log(jessica2);
+console.log(jessicaCopy);
+
+jessicaCopy.family.push('Mary','John');
+
+
+
+
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,14 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function(starterIndex, mainIndex){
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  },
+
+  orderDelivery: function({starterIndex = 1, mainIndex = 0, time = '20:00', address}){
+    console.log(`order received: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +35,78 @@ const restaurant = {
     },
   },
 };
+
+restaurant.orderDelivery({
+  time:'22:30',
+  address: 'duque de caxias 1338',
+  mainIndex: 2,
+  starterIndex: 2,
+})
+
+
+//object destructuring
+const {name, openingHours, categories} = restaurant
+
+console.log(name, openingHours, categories);
+
+const {name: restaurantName, openingHours: hours, categories: tags} = restaurant;
+console.log(restaurantName, hours, tags);
+
+const {menu = [], starterMenu: starters = []} = restaurant
+
+console.log(menu, starters);
+
+//mutating variables
+let s = 111;
+let t = 999;
+const obj = {s: 23, t: 7, u: 14};
+
+({s, t} = obj);
+console.log(s, t);
+
+//nested objects
+const {fri: {open: o, close: d}} = openingHours
+console.log(o, d);
+
+
+
+
+
+
+
+
+//descruturing is unpack values of and array into separable variables, breakdown a complex structure into a smaller structure
+
+const arr = [2,3,4];
+
+//without destructuring
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+//with descruturing; square brackets on the left side
+const [x,y,z] = arr;
+console.log(x,y,z);
+console.log(arr);
+
+let [main, ,secondary] = restaurant.categories
+console.log(main, secondary);
+
+//const temp = main;
+//main = secondary;
+//secondary = temp;
+
+//console.log(main, secondary);
+
+[secondary, main] = restaurant.order(2, 0)
+console.log(main, secondary);
+
+//nested destructuring
+const nested = [2,4, [5, 6]];
+
+const [i, ,[j,k]] = nested;
+console.log(i, j, k);
+
+//default values
+const [p = 1,q = 1,r = 1] = [8,9];
+console.log(p,q,r);
